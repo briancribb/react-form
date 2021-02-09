4e40bb11bafe57aa05b7154b006b29741d152257
Brian Cribb <Brian.Cribb@rws.com>
2021-02-09 11:55:54 -0500

    AUR new folders

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import someThing from "./extra.js";
+
+let hello = "Hello";
+var world = "World";
+var stuff = "things";
+var myArray = [1,2,3];
+Array.from(myArray).forEach((item) =>{
+	console.log(item);
+});
+console.log('someThing', someThing);
+
+let helloWorld = ()=> {
+	console.log('stuff', stuff);
+	return hello + ', ' + world + '!';
+}
+let objBlah = {
+  blah:'blah'
+}
+/*
+This is a comment
+*/
+helloWorld();
+console.log(helloWorld()); // and stuff
